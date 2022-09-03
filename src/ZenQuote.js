0003a2968b9e00b199c2be20c5e6202c5f46e515
Sven Zaugg <mogwai.dev@gmail.com>
2022-09-03 15:29:27 +0200

    CORS proxy server changed

--- src/ZenQuote.js
+++ src/ZenQuote.js
@@ -2,7 +2,7 @@ import "./ZenQuote.css";
 import React from "react";
 
 class ZenQuote extends React.Component {
-  CORS_PROXY_URI = "https://thingproxy.freeboard.io/fetch";
+  CORS_PROXY_URI = "https://api.codetabs.com/v1/proxy?quest=";
   ZEN_QUOTES_REST_API_URI = "https://zenquotes.io/api/quotes";
 
   constructor(props) {
@@ -29,7 +29,7 @@ class ZenQuote extends React.Component {
   }
 
   fetchQuotes() {
-    fetch(this.CORS_PROXY_URI + '/' + this.ZEN_QUOTES_REST_API_URI)
+    fetch(this.CORS_PROXY_URI + this.ZEN_QUOTES_REST_API_URI)
       .then((response) => response.json())
       .then((json) => JSON.parse(JSON.stringify(json)))
       .then((quotes) => this.setState({ quotes: quotes }))
