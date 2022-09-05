3851aaa17d5b08db840ce65ee99c8eedeb053de9
Sven Zaugg <mogwai.dev@gmail.com>
2022-09-05 22:58:02 +0200

    Material UI/Emotion integrated & ContactForm component refactored with the use of MUI

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,7 +1,13 @@
 import React, { useState, useRef } from "react";
-import "./ContactForm.css";
 import emailjs from "@emailjs/browser";
 import ReCAPTCHA from "react-google-recaptcha";
+import { Button, TextField, InputAdornment } from "@mui/material";
+import {
+  PersonOutlineRounded,
+  AlternateEmailRounded,
+  MailOutlineRounded,
+  PhoneAndroidRounded,
+} from "@mui/icons-material";
 
 const ContactForm = () => {
   const EMAILJS_SERVICE_ID = "service_863d7z6";
@@ -73,6 +79,8 @@ const ContactForm = () => {
       )}
 
       <form
+        style={{ position: "left", width: "300px" }}
+        className="contact-form"
         ref={formRef}
         onSubmit={(event) => {
           handleSubmit(event);
@@ -80,25 +88,105 @@ const ContactForm = () => {
       >
         <input type="hidden" name="contact_number" />
         <div>
-          <input
-            type="text"
-            placeholder="Dein Name"
+          <TextField
+            id="user_name"
             name="user_name"
+            label="Name"
             required
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PersonOutlineRounded />
+                </InputAdornment>
+              ),
+            }}
+            size="small"
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            className="contact-form-input"
           />
         </div>
         <div>
-          <input type="email" placeholder="Email" name="user_email" required />
+          <TextField
+            id="user_email"
+            name="user_email"
+            label="E-Mail"
+            type="email"
+            required
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AlternateEmailRounded />
+                </InputAdornment>
+              ),
+            }}
+            size="small"
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            className="contact-form-input"
+          />
         </div>
         <div>
-          <textarea placeholder="Dein Anliegen" name="message" required />
+          <TextField
+            id="user_phone"
+            name="user_phone"
+            label="Mobile"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PhoneAndroidRounded />
+                </InputAdornment>
+              ),
+            }}
+            size="small"
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            className="contact-form-input"
+          />
+        </div>
+        <div>
+          <TextField
+            id="message"
+            name="message"
+            label="Anliegen"
+            required
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <MailOutlineRounded />
+                </InputAdornment>
+              ),
+            }}
+            multiline
+            margin="dense"
+            rows={5}
+            variant="outlined"
+            fullWidth
+            className="contact-form-input"
+          />
         </div>
         <div>
           {/* reCAPTCHA's server sitekey is saved under email templates of emailjs */}
           <ReCAPTCHA sitekey="6LdOo9AhAAAAAOWUQaIemW7skUKng73JLFt0lI-8" />
         </div>
         <div>
-          <button type="submit">Anfrage senden!</button>
+          <Button
+            variant="contained"
+            size="small"
+            type="submit"
+            sx={{
+              backgroundColor: "#6d034b",
+              "&:hover": {
+                backgroundColor: "#a41b6b",
+              },
+              marginTop: "10px"
+            }}
+          >
+            Anfrage senden!
+          </Button>
         </div>
       </form>
     </div>
