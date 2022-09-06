cc92cd0566f35f49fc6eccc122a43c553a58a4ba
Sven Zaugg <mogwai.dev@gmail.com>
2022-09-06 13:28:29 +0200

    react-carousel-minimal library integrated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,76 @@
 import NavigationBar from "./NavigationBar";
 import ZenQuote from "./ZenQuote";
+import { Carousel } from 'react-carousel-minimal';
 import ContactForm from "./ContactForm";
 import CompanyLogo from "./el-taurus.png";
 import "./App.css";
 
 function App() {
+  const data = [
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+      caption: `<div>
+                  San Francisco
+                  <br/>
+                  Next line
+                </div>`,
+    },
+    {
+      image:
+        "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+      caption: "Scotland",
+    },
+    {
+      image: CompanyLogo,
+      caption: "El Taurus",
+    },
+    {
+      image:
+        "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
+      caption: "Darjeeling",
+    },
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
+      caption: "San Francisco",
+    },
+    {
+      image:
+        "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
+      caption: "Scotland",
+    },
+    {
+      image:
+        "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
+      caption: "Darjeeling",
+    },
+    {
+      image:
+        "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
+      caption: "San Francisco",
+    },
+    {
+      image:
+        "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
+      caption: "Scotland",
+    },
+    {
+      image:
+        "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
+      caption: "Darjeeling",
+    },
+  ];
+
+  const captionStyle = {
+    fontSize: "2em",
+    fontWeight: "bold",
+  };
+  const slideNumberStyle = {
+    fontSize: "20px",
+    fontWeight: "bold",
+  };
+
   return (
     <div className="App">
       <div className="App-header">
@@ -29,6 +95,33 @@ function App() {
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
           including versions of Lorem Ipsum.
+          <div>
+            <Carousel
+              data={data}
+              time={3000}
+              width="850px"
+              height="500px"
+              captionStyle={captionStyle}
+              radius="10px"
+              slideNumber={false}
+              slideNumberStyle={slideNumberStyle}
+              captionPosition="bottom"
+              automatic={true}
+              dots={true}
+              pauseIconColor="white"
+              pauseIconSize="40px"
+              slideBackgroundColor="darkgrey"
+              slideImageFit="cover"
+              thumbnails={false}
+              thumbnailWidth="100px"
+              style={{
+                textAlign: "left",
+                maxWidth: "850px",
+                maxHeight: "500px",
+                marginTop: "15px"
+              }}
+            />
+          </div>
         </article>
         <article>
           <h1 id="you-read">Du liest</h1>
